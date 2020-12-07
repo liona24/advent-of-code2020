67031c7576b5b2cdced6cc3019b4e8fb8eaf0383
Lion Ackermann <ackerm.lion@hotmail.de>
2020-12-07 21:21:01 +0100

    Day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,5 +1,9 @@
+use std::collections::HashSet;
+
+use crate::util::read_lines;
+
 pub fn solve() {
-    println!("Day 3 - Part 1");
+    println!("Day 6 - Part 1");
     solve_first();
     println!("      - Part 2");
     solve_second();
@@ -7,9 +11,50 @@ pub fn solve() {
 }
 
 fn solve_first() {
+    let input = read_lines("inputs/d06/input.txt").expect("Could not find input for day 6!");
 
+    let mut groups = vec![HashSet::new()];
+
+    for line in input.filter_map(|line| line.ok()) {
+        if line.is_empty() {
+            groups.push(HashSet::new());
+            continue;
+        }
+        for c in line.chars() {
+            groups.last_mut().unwrap().insert(c);
+        }
+    }
+
+    let ans = groups.iter().fold(0, |sum, set| sum + set.len());
+    println!("The solution is {}", ans);
 }
 
 fn solve_second() {
+    let input = read_lines("inputs/d06/input.txt").expect("Could not find input for day 6!");
 
+    let mut groups = Vec::new();
+    let mut current_group: Option<HashSet<_>> = None;
+
+    for line in input.filter_map(|line| line.ok()) {
+        if line.is_empty() {
+            groups.push(current_group.unwrap());
+            current_group = None;
+            continue;
+        }
+
+        let chars: HashSet<_> = line.chars().collect();
+
+        current_group = if let Some(set) = current_group {
+            Some(set.intersection(&chars).cloned().collect())
+        } else {
+            Some(chars)
+        }
+    }
+
+    if let Some(current_group) = current_group {
+        groups.push(current_group);
+    }
+
+    let ans = groups.iter().fold(0, |sum, set| sum + set.len());
+    println!("The solution is {}", ans);
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 fn main() {
     day01::solve();
@@ -12,4 +13,5 @@ fn main() {
     day03::solve();
     day04::solve();
     day05::solve();
+    day06::solve();
 }
