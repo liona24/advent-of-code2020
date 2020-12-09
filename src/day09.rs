1bc117800b9a9b51d2b97d132c30f72c667e76c0
Lion Ackermann <ackerm.lion@hotmail.de>
2020-12-09 08:53:34 +0100

    Day 9

--- src/day09.rs
+++ src/day09.rs
@@ -1,15 +1,76 @@
+use std::collections::VecDeque;
+
+use crate::util::read_lines;
+
 pub fn solve() {
-    println!("Day 3 - Part 1");
-    solve_first();
+    println!("Day 9 - Part 1");
+    let n = solve_first().expect("Should have found a solution!");
     println!("      - Part 2");
-    solve_second();
+    solve_second(n);
     println!();
 }
 
-fn solve_first() {
+fn solve_first() -> Option<usize> {
+    let input = read_lines("inputs/d09/input.txt").expect("Could not find input for day 9!");
 
+    let mut numbers = input
+        .filter_map(|line| line.ok())
+        .map(|line| line.parse::<usize>().expect("Input should be number!"));
+
+    let mut preamble: VecDeque<_> = numbers.by_ref().take(25).collect();
+
+    for n in numbers {
+        let mut found = false;
+        for i in 0..preamble.len() - 1 {
+            for j in i + 1..preamble.len() {
+                if preamble[i] != preamble[j] && preamble[i] + preamble[j] == n {
+                    found = true;
+                    break;
+                }
+            }
+            if found {
+                break;
+            }
+        }
+
+        if !found {
+            println!("The solution is {}", n);
+            return Some(n);
+        }
+
+        preamble.pop_front();
+        preamble.push_back(n);
+    }
+
+    None
 }
 
-fn solve_second() {
+fn solve_second(first_solution: usize) {
+    let input = read_lines("inputs/d09/input.txt").expect("Could not find input for day 9!");
 
+    let numbers = input
+        .filter_map(|line| line.ok())
+        .map(|line| line.parse::<usize>().expect("Input should be number!"));
+
+    let mut sum = 0;
+    let mut set = VecDeque::new();
+
+    for n in numbers {
+        sum += n;
+        set.push_back(n);
+
+        while sum > first_solution {
+            if let Some(i) = set.pop_front() {
+                sum -= i;
+            } else {
+                break;
+            }
+        }
+
+        if set.len() >= 2 && sum == first_solution {
+            let ans = set.iter().min().unwrap() + set.iter().max().unwrap();
+            println!("The solution is {}", ans);
+            break;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod day05;
 mod day06;
 mod day07;
 mod day08;
+mod day09;
 
 fn main() {
     day01::solve();
@@ -18,4 +19,5 @@ fn main() {
     day06::solve();
     day07::solve();
     day08::solve();
+    day09::solve();
 }
