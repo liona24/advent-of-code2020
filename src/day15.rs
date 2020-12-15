54957f745dd9693d52f01a8e8a1ce97628c0457d
Lion Ackermann <ackerm.lion@hotmail.de>
2020-12-15 17:25:19 +0100

    Day 15

--- src/day15.rs
+++ src/day15.rs
@@ -1,15 +1,43 @@
+use std::collections::HashMap;
+
 pub fn solve() {
-    println!("Day 3 - Part 1");
-    solve_first();
+    const INPUT: [usize; 7] = [9, 19, 1, 6, 0, 5, 4];
+
+    println!("Day 15 - Part 1");
+    solve_chall(&INPUT, 2020);
     println!("      - Part 2");
-    solve_second();
+    if cfg!(release) {
+        solve_chall(&INPUT, 30000000);
+    } else {
+        println!("Skipped.");
+    }
     println!();
 }
 
-fn solve_first() {
+fn solve_chall(input: &[usize], n: usize) {
+    let mut age = 0;
+    let mut map = HashMap::new();
 
-}
+    let mut input = input.iter();
+    let mut last_spoken = *input.next().expect("at least one number required!");
 
-fn solve_second() {
+    for i in input {
+        map.insert(last_spoken, age);
+        last_spoken = *i;
+        age += 1;
+    }
 
+    while age < n - 1 {
+        let next = if let Some(t_last) = map.get(&last_spoken) {
+            age - t_last
+        } else {
+            0
+        };
+        map.insert(last_spoken, age);
+        last_spoken = next;
+
+        age += 1;
+    }
+
+    println!("The solution is {}", last_spoken);
 }
